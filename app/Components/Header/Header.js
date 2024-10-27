e648dc94e2918f7c5d7ff6f4538368b51c0ce718
Alex Kuznecov <alexander.kuznecov16@gmail.com>
2024-10-27 12:18:18 +0200

    portfolio

---
+++ app/Components/Header/Header.js
@@ -0,0 +1,50 @@
+'use client';
+import React, {useRef, useState} from 'react';
+import './Header.css';
+import {IoMdMenu, IoMdClose} from 'react-icons/io';
+import Link from 'next/link';
+
+export default function Header() {
+	const mobileRef = useRef(null);
+	const [isMobileOpen, setMobileOpen] = useState(false);
+
+	const openMobile = () => {
+		setMobileOpen(true);
+		document.body.style.overflow = 'hidden';
+	};
+
+	const closeMobile = () => {
+		setMobileOpen(false);
+		document.body.style.overflow = '';
+	};
+
+	return (
+		<header className='header'>
+			<div className='container'>
+				<nav className='header__nav full'>
+					<Link href='/'>Overview</Link>
+					<Link href='/programming'>Programming</Link>
+					<Link href='/profile'>Profile</Link>
+					<Link href='/contacts'>Get in Touch</Link>
+				</nav>
+
+				<IoMdMenu className='toOpen' onClick={openMobile} />
+				<nav ref={mobileRef} className={`header__nav mobile ${isMobileOpen ? 'opened' : ''}`}>
+					<IoMdClose className='toClose' onClick={closeMobile} />
+					<Link onClick={closeMobile} href='/'>
+						Overview
+					</Link>
+					<Link onClick={closeMobile} href='/programming'>
+						Programming
+					</Link>
+					<Link onClick={closeMobile} href='/profile'>
+						Profile
+					</Link>
+					<Link onClick={closeMobile} href='/contacts'>
+						Get in Touch
+					</Link>
+				</nav>
+			</div>
+		</header>
+	);
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,82 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from 'next/link';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import './main.css';
+import Image from 'next/image';
+import {SiThealgorithms} from 'react-icons/si';
+import {FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<div className='intro'>
+				<div className='container'>
+					<div className='intro__inner'>
+						<span>I am</span>
+						<h1 className='intro-title'>Alexander Kuznecov</h1>
+						<p>Web developer</p>
+						<a href='https://github.com/alexkuznecov16'>Github</a>
+					</div>
+				</div>
+				<div class='mouse'></div>
+			</div>
+			<main className='Main'>
+				<div className='container'>
+					<div className='Main__inner'>
+						<div className='programming'>
+							<h4 className='title'>Programming</h4>
+							<div className='programming__inner'>
+								<div className='programming-item'>
+									<SiThealgorithms />
+									<Link href='/programming/algorithms'>Algorithms</Link>
+								</div>
+								<div className='programming-item'>
+									<SiThealgorithms />
+									<Link href='/programming/frontend'>Frontend</Link>
+								</div>
+								<div className='programming-item'>
+									<SiThealgorithms />
+									<Link href='/programming/projects'>Projects</Link>
+								</div>
+							</div>
+							<Link className='more' href='/programming'>
+								Full programming
+							</Link>
+						</div>
+						<div className='about'>
+							<h4 className='title'>Profile</h4>
+							<div className='about__inner'>
+								<p className='text'>
+									Hello! My name is Alex, am a passionate Web developer. My programming journey began in 2020. It was the year I was introduced to technologies like Python, Unity, Unreal Engine, HTML CSS. Each of these technologies had strengths in specific areas, but HTML and CSS stood out to me as the most compelling. Since then, I have been diving deeper intro web development.
+								</p>
+								<Image src='/my-image.jpg' width={400} height={450} alt='my-image' />
+							</div>
+							<Link className='more' href='/profile'>
+								Full profile
+							</Link>
+						</div>
+						<div className='contacts'>
+							<h4 className='title'>Get in Touch</h4>
+							<div className='contacts__inner'>
+								<Link className='contact-item instagram' href='https://www.instagram.com/kuznecov_alex_?igsh=emR2dGh0MHQ1Z3d3' target='_blank'>
+									<FaInstagram />
+								</Link>
+								<Link className='contact-item linkedin' href='https://www.linkedin.com/in/alexander-kuznecov' target='_blank'>
+									<FaLinkedin />
+								</Link>
+								<Link className='contact-item github' href='https://github.com/alexkuznecov16' target='_blank'>
+									<FaGithub />
+								</Link>
+							</div>
+							<Link className='more' href='/contacts'>
+								Full contacts
+							</Link>
+						</div>
+					</div>
+				</div>
+			</main>
+			<Footer />
+		</>
+	);
 }
